0009d8f3e03f1635fbc5d5b48d46023a7752c7f3
mathematical-application-3607 <hannamarie.mehmet@wework.com>
2019-03-25 18:40:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function fetchBooks() {
   //write fetch request to the Game of Thrones API
-  fetch('https://anapioficeandfire.com/api/books/5')
+  fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
   .then(json => renderBooks());
 }
